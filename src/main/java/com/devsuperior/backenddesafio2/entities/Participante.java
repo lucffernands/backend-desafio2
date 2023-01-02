0589d0375bce968f60554bca07f400f65894fd30
Luciano Fernandes <lucianoffernands@gmail.com>
2023-01-02 10:19:37 -0300

    Criada a classe Participante

---
+++ src/main/java/com/devsuperior/backenddesafio2/entities/Participante.java
@@ -0,0 +1,44 @@
+package com.devsuperior.backenddesafio2.entities;
+
+import jakarta.persistence.Column;
+
+public class Participante {
+
+    private Integer id;
+    private String nome;
+    @Column(unique = true)
+    private String email;
+
+    public Participante() {
+    }
+
+    public Participante(Integer id, String nome, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
