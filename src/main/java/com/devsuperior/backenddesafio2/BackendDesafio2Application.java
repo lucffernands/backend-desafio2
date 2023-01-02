bc1ad5952e01461c73ba8e1a24947b390a6ae4d9
Luciano Fernandes <lucianoffernands@gmail.com>
2023-01-02 10:11:14 -0300

    Criada a classe Categoria

---
+++ src/main/java/com/devsuperior/backenddesafio2/BackendDesafio2Application.java
@@ -0,0 +1,13 @@
+package com.devsuperior.backenddesafio2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDesafio2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDesafio2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/backenddesafio2/BackendDesafio2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.backenddesafio2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDesafio2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
